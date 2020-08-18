8f6c789b688451034888d8cdbbb63f6d63777b19
Nazarii Skoropad <nazar.skoropad@icloud.com>
2020-08-18 14:15:31 +0300

    Create UserAnswersDto

--- QuizApplication.Backend/API Contracts/QuizApplication.Contracts/DTOs/QuestionDto.cs
+++ QuizApplication.Backend/API Contracts/QuizApplication.Contracts/DTOs/QuestionDto.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace QuizApplication.Contracts.DTOs
 {
-    public class QuestionDto 
+    public class QuestionDto : BaseDto
     {
         public int TestId { get; set; }
 
---
+++ QuizApplication.Backend/API Contracts/QuizApplication.Contracts/DTOs/UserAnswersDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApplication.Contracts.DTOs
+{
+    public class UserAnswersDto
+    {
+        public string UserName { get; set; }
+        public ICollection<QApair> QApairs { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime EndedAt { get; set; }
+    }
+
+    public class QApair
+    {
+        public int QuestionId { get; set; }
+        public int AnswerId { get; set; }
+    }
+}
