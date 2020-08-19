ef3e7b244484d96591c645db06d9cc397ec8d0ce
Nazarii Skoropad <nazar.skoropad@icloud.com>
2020-08-20 01:03:20 +0300

    Add validation to UserAnswersDto

--- QuizApplication.Backend/API Contracts/QuizApplication.Contracts/DTOs/UserAnswersDto.cs
+++ QuizApplication.Backend/API Contracts/QuizApplication.Contracts/DTOs/UserAnswersDto.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿using QuizApplication.Contracts.Constants;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace QuizApplication.Contracts.DTOs
 {
     public class UserAnswersDto
     {
+        [Required]
+        [StringLength(PropertyConstrains.UserNameLength)]
         public string UserName { get; set; }
+
+        [Required]
         public ICollection<QApair> QApairs { get; set; }
+
+        [Required]
         public DateTime StartedAt { get; set; }
+
+        [Required]
         public DateTime EndedAt { get; set; }
     }
 
