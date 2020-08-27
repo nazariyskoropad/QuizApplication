50c0b34ad0dd0775837e2fb94f02f6cf5bddbfb6
Nazarii Skoropad <nazar.skoropad@icloud.com>
2020-08-27 16:31:22 +0300

    Create refresh token config and migration

--- QuizApplication.Backend/API Contracts/QuizApplication.Contracts/Entities/RefreshToken.cs
+++ QuizApplication.Backend/API Contracts/QuizApplication.Contracts/Entities/RefreshToken.cs
@@ -11,7 +11,6 @@ namespace QuizApplication.Contracts.Entities
         public DateTime Expires { get; set; }
         public bool IsExpired => DateTime.UtcNow >= Expires;
         public DateTime Created { get; set; }
-        public string CreatedByIp { get; set; }
         public DateTime? Revoked { get; set; }
         public string ReplacedByToken { get; set; }
         public bool IsActive => Revoked == null && !IsExpired;
---
+++ QuizApplication.Backend/API Infrastructure/QuizApplication.Infrastructure.AppContext/Persistence/Configuration/RefreshTokenConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuizApplication.Contracts.Entities;
+
+namespace QuizApplication.Infrastructure.AppContext.Persistence.Configuration
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(rt => rt.Id);
+            builder.HasOne(rt => rt.Admin)
+                .WithMany(a => a.RefreshTokens)
+                .HasForeignKey(rt => rt.AdminId);
+        }
+    }
+}
