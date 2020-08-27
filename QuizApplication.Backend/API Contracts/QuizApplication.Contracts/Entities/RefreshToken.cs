dad9d8fbeba7edc0337aeeb5c9b2ee4dbc24423e
Nazarii Skoropad <nazar.skoropad@icloud.com>
2020-08-27 16:30:23 +0300

    Create refresh token entity

---
+++ QuizApplication.Backend/API Contracts/QuizApplication.Contracts/Entities/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace QuizApplication.Contracts.Entities
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public int AdminId { get; set; }
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public DateTime Created { get; set; }
+        public string CreatedByIp { get; set; }
+        public DateTime? Revoked { get; set; }
+        public string ReplacedByToken { get; set; }
+        public bool IsActive => Revoked == null && !IsExpired;
+        public Admin Admin { get; set; }
+    }
+}
