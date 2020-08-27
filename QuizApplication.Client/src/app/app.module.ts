b27a4b37a354db4185bb32ba1fc6fcd204ee777d
Nazarii Skoropad <nazar.skoropad@icloud.com>
2020-08-27 16:39:16 +0300

    Register all components and modules.

--- QuizApplication.Client/src/app/app.module.ts
+++ QuizApplication.Client/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { appInitializer } from './helpers/app.initializar';
+import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [        
+    { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AuthenticationService] },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
